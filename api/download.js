3695013100a4f1e21bde12bd69715b77602e6305
KomaVR <komavr12454@gmail.com>
2025-08-24 15:44:03 -0700

    Create download.js

---
+++ api/download.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+import path from "path";
+
+export default function handler(req, res) {
+    const bundlePath = path.join(process.cwd(), "AssetBundles", "modassets");
+    if (!fs.existsSync(bundlePath)) {
+        return res.status(404).send("AssetBundle not found");
+    }
+
+    const fileBuffer = fs.readFileSync(bundlePath);
+
+    res.setHeader("Content-Type", "application/octet-stream");
+    res.setHeader("Content-Disposition", "attachment; filename=modassets");
+    res.status(200).send(fileBuffer);
+}
